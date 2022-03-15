a1008a2bd7bae6ab01ded56276f0459ac8d21715
EdithPink <pinkrahedith@gmail.com>
2022-03-15 16:56:41 -0700

    putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * main - Print the word "_putchar"
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char str[] = "_putchar";
+	int i;
+
+	i = 0;
+	while (str[i])
+	{
+		_putchar(str[i]);
+		++i;
+	}
+	_putchar('\n');
+	return (0);
+}

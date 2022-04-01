5b0196abd647938d19312d5814ebadae6148306d
EdithPink <pinkrahedith@gmail.com>
2022-04-01 15:04:54 -0700

    add 4

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * main - adds two positive numbers
+ * @argc: number of arguments
+ * @argv: array of pointers
+ *
+ * Return: 0
+ */
+
+int main(int argc, char **argv)
+{
+	int j = 1, sum = 0;
+
+	if (argc > 1)
+	{
+		for (; j < argc; j++)
+		{
+			if (checker(argv[j]))
+				sum += atoi(argv[j]);
+			else
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+	}
+	printf("%d\n", sum);
+	return (0);
+}
